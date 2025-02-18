d94e6413335c33a786acd2d1ed846c95fe9193a3
PaWaN-1309 <pawanmalgavi@gmail.com>
2025-02-18 16:57:12 +0530

    Initial commit

---
+++ backend/src/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoURI = process.env.MONGODB_URL;
+async function connectToMongo() {
+  await mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected to Mongo Successfully"))
+    .catch((err) => console.log(err));
+}
+module.exports = connectToMongo;
