d830d33d9e11eea18b4400eadae77b3ee34f8a07
PaWaN-1309 <pawanmalgavi@gmail.com>
2025-02-19 14:10:56 +0530

    Feat(Change Password): API Route for change password
    - Forgot Password & Change Password both can be done using same /change-password route

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -17,8 +17,7 @@ const registerUser = async (req, res) => {
     newUser.password = await bcrypt.hash(password, salt); // ! Hash the password
     await newUser.save(); // ! Save the user to the database
     res.status(201).json({ message: "User registered successfully" });
-  } 
-  catch (error) {
+  } catch (error) {
     next(error);
   }
 };
@@ -30,12 +29,12 @@ const loginUser = async (req, res) => {
     // ! Check if user exists
     const user = await User.findOne({ email }).select("+password");
     if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "User does not exists" });
     }
     // ! Check if password is correct
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Invalid password" });
     }
     const payload = {
       user: {
@@ -43,11 +42,57 @@ const loginUser = async (req, res) => {
       },
     };
     // ! Generate a token & send with the user id
-    const token = generateToken(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = generateToken(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
     res.status(200).json({ message: "Logged in successfully", token });
-  } 
-  catch (error) {
+  } catch (error) {
     next(error);
+  }
+};
+
+// ? Change password
+const changePassword = async (req, res, next) => {
+  const { purpose } = req.body;
+  if (purpose === "change") {
+    const { email, oldPassword, newPassword } = req.body;
+    try {
+      // ! Check if user exists
+      const user = await User.findOne({ email }).select("+password");
+      if (!user) {
+        return res.status(400).json({ message: "User does not exists" });
+      }
+      // ! Check if old password is correct
+      const isMatch = await bcrypt.compare(oldPassword, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid password" });
+      }
+      const salt = await bcrypt.genSalt(10); // ! Salt to hash the password
+      user.password = await bcrypt.hash(newPassword, salt); // ! Hash the new password
+      await user.save(); // ! Save the user to the database
+      res.status(200).json({ message: "Password changed successfully" });
+    } catch (error) {
+      next(error);
+    }
+  } else if (purpose === "forgot") {
+    const { email, password } = req.body;
+    try {
+      // ! Check if user exists
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "User does not exists" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res
+        .status(200)
+        .json({ message: "New password has been set successfully" });
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    res.status(400).json({ message: "Invalid purpose" });
   }
 };
 
@@ -57,5 +102,6 @@ const logoutUser = (req, res) => {};
 module.exports = {
   registerUser,
   loginUser,
+  changePassword,
   logoutUser,
 };
