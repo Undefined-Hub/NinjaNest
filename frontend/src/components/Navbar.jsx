a7e2558667e2f775e4e66a41fc0aa0219bd29ad0
arpatil-dev <aryanrpatil2023@gmail.com>
2025-02-22 18:14:57 +0530

    Style(Navbar-Footer): Added Navbar and Footer

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
-    const navigate = useNavigate();
-    return (
-        <header className="text-gray-600 body-font px-7 sticky top-0 z-50 backdrop-blur-lg bg-white/70">
-            <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center text-gray-900 md:mb-0">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg>
-                    <span onClick={() => navigate('/')} className="ml-3 text-xl cursor-pointer">NinjaNest</span>
-                </a>
-                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a className="mr-5 hover:text-gray-900 hover:cursor-pointer" onClick={() => navigate('/')}>Home</a>
-                    <a className="mr-5 hover:text-gray-900 hover:cursor-pointer" onClick={() => navigate('/')}>About</a>
-                    <a className="mr-5 hover:text-gray-900 hover:cursor-pointer" onClick={() => navigate('/')}>Contact Us</a>
-                    <a className="mr-5 hover:text-gray-900 hover:cursor-pointer" onClick={() => navigate('/auth')}>Login / Signup</a>
-                </nav>
-            </div>
-        </header>
-    )
-}
+  const navigate = useNavigate();
+return (
+    <header className="bg-[#111827] flex justify-between items-center text-white p-3">
+        <div className="text-2xl bg-gradient-to-r from-[#7c3bf1] to-[#2761e9] bg-clip-text text-transparent font-bold">NinjaNest</div>
+        <nav className="flex space-x-4 text-[#727986] font-bold font-mono">
+            {['Home', 'Search', 'Features', 'Contact'].map((item) => (
+                <li key={item} className="list-none">{item}</li>
+            ))}
+        </nav>
+        <div className="flex gap-4 text-md">
+            <button className="text-[#fbfbfb] p-2 px-4 bg-[#18212f] rounded-lg font-bold">Sign In</button>
+            <button className="text-[#fbfbfb] p-2 px-4 bg-gradient-to-r from-[#7c3bf1] to-[#2761e9] rounded-lg font-bold">Sign Up</button>
+        </div>
+    </header>
+);
+};
 
-export default Navbar
+export default Navbar;
